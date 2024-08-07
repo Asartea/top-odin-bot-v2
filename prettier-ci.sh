6304314f6c0366ce8ee87acdba57fba0b481177d
Asartea <76259120+Asartea@users.noreply.github.com>
2024-08-07 18:45:40 +0200

    Fix: Run prettier only once
    Previously, the script ran through the list and then used `xargs` to run prettier for each individual file. This theoretically is more durable, but has the annoying sideffect of generating log output.
    This change collects the files to be checked, and then run prettier over the entire list

--- prettier-ci.sh
+++ prettier-ci.sh
@@ -5,18 +5,23 @@
 # Get the list of changed files
 changed_files=$(git diff --name-status main)
 
+# Create an empty array to store the file paths
+files_to_format=()
+
 # Iterate over the list of changed files
-echo "$changed_files" | while IFS=$'\t' read -r file; do
-    # Get the mode and file path
+while read -r file; do
     mode=$(echo "$file" | cut -f1)
     filePath=$(echo "$file" | cut -f2)
-    # Check if the file is modified or added
     if [ "$mode" = "M" ] || [ "$mode" = "A" ] || [ "$mode" = "AM" ]; then
-        # Check if the file extension is .js, .json, .yml, or .yaml
         case "$filePath" in
             *.js|*.json|*.yml|*.yaml)
-                npx prettier --write "$filePath"
+                files_to_format+=("$filePath")
                 ;;
         esac
     fi
-done
+    # Check if the file extension is .js, .json, .yml, or .yaml
+done <<< "$changed_files"
+
+echo "Files to format: ${files_to_format[@]}"
+# Run prettier on each file in the array
+npx prettier --check "${files_to_format[@]}"
