4ef2c16983ec2e1f6081bb027444934b7aafa271
Asartea <76259120+Asartea@users.noreply.github.com>
2024-08-07 15:17:39 +0200

    fix errors

--- prettier-ci.sh
+++ prettier-ci.sh
@@ -5,18 +5,20 @@
 # Get the list of changed files
 changed_files=$(git diff --name-status main)
 
-# Loop through the list of changed files
-echo "$changed_files" | while read -r file; do
+# Iterate over the list of changed files
+echo "$changed_files" | while IFS=$'\t' read -r file; do
     # Get the mode and file path
     mode=$(echo "$file" | cut -f1)
     filePath=$(echo "$file" | cut -f2)
     # Check if the file is modified or added
     if [ "$mode" = "M" ] || [ "$mode" = "A" ] || [ "$mode" = "AM" ]; then
         # Check if the file extension is .js, .json, .yml, or .yaml
-        if [[ "$filePath" == *.js || "$filePath" == *.json || "$filePath" == *.yml || "$filePath" == *.yaml ]]; then
-            # Run prettier on the file
-            npx prettier --write "$filePath"
-        fi
+        case "$filePath" in
+            *.js|*.json|*.yml|*.yaml)
+                npx prettier --write "$filePath"
+                ;;
+            *)
+                ;;
+        esac
     fi
-done
 done
